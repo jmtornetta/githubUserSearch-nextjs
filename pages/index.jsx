fee074cdf122cb6baa48a2f26436b77ab42d04f1
Jon Tornetta <jmtornetta@gmail.com>
2022-08-03 00:43:58 -0400

    Feature: Add instant search with throttle.

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,25 +7,56 @@ import { useState } from "react"
 // import { token } from "/token.js" // For testing and development
 
 export default function SearchGithub() {
-  const token = process.env.GITHUB_AUTH
-  console.log(process.env.TEST_VAR)
-  
   /* App configuration and constants */
+  const token = process.env.GITHUB_AUTH
   const fetchOptions = {
     headers: {
       Authorization: `token ${token}`
     }
   } // Posterity: Including different header options seems to cause a cors issue
   const perPage = 30 // Later can make dynamic with: [perPage,setPerPage] = useState(100)
+  const maxResults = 1000 // From GitHub search API spec
+
   const [results, setResultsArr] = useState({
     query: null,
     pageIndex: null,
     items: [],
-    overflow: false
+    overflow: false,
+    limits: [30, 30]
   })
-  const maxResults = 1000 // From GitHub search API spec
 
   /* App logic */
+  const throttle = (() => {
+    let runs = 0
+    return (fn, time, ...args) => {
+      runs += 1
+      const currentRun = runs
+      setTimeout(() => {
+        if (currentRun === runs) fn(...args)
+      }, time)
+    }
+  })()
+
+  const limitAndThrottle = (remaining, limit, delayTime, fn, ...args) => {
+    if (remaining === 0) { alert("Exceeded limit! Wait a minute."); return }
+    // runCount += 1 // Posterity: Using rate limit API instead as stopper for simplicity.
+
+    let time
+    const left = remaining / limit
+    if (left > .8) time = delayTime
+    else if (left > .6) time = delayTime * 2
+    else if (left > .4) time = delayTime * 4
+    else if (left > .2) time = delayTime * 8
+    else time = delayTime * 16
+
+    throttle(fn, time, ...args)
+  }
+
+  const getGithubLimits = async (endpoint = `https://api.github.com/rate_limit`, options = fetchOptions) => {
+    const limits = await fetch(endpoint, options).then(response => response.json())
+    return [limits.resources.search.remaining, limits.resources.search.limit]
+  }
+
   const fetchGithub = async (query, pageIndex = 0, perPage = perPage, sortMethod, type = "users") => {
     if (!query) { return }
     const endpoint = `https://api.github.com/search/${type}`
@@ -43,11 +74,10 @@ export default function SearchGithub() {
       const allData = await Promise.all(data.items.map(async (user) => {
         const userDetails = await fetch(user.url, fetchOptions).then(response => response.json()) // Schema: Object of user detail properties
         user = { ...user, ...userDetails }
-        console.log(user)
         // user.repos = await fetch(user.repos_url).then(response => response.json())// Schema: Array of repo objects
         return user
       }))
-      output = { items: allData, totalCount: data.total_count, overflow: data.overflow, incompleteResults: data.incomplete_results }
+      output = { items: allData, totalCount: data.total_count, overflow: data.overflow, incompleteResults: data.incomplete_results, limits: await getGithubLimits() }
     } catch (err) {
       console.error(`GitHub fetch failed: ${err}`)
       alert(err)
@@ -59,7 +89,6 @@ export default function SearchGithub() {
   }
 
   const handlePageChange = (event) => {
-    // [ ] Set timeout so fetch only occurs if timeout has a "true" value  
     // Set new current page, show first page, last page, previous 3 pages and next 3 pages; Add links for page buttons
     const newPage = event.selected // "Event" has a custom method called selected which equal the index of the page array
     fetchGithub(results.query, newPage) // Fetch again with same query but change page results
@@ -103,33 +132,40 @@ export default function SearchGithub() {
   return (
     <Layout title="GitHub User Search">
       <p>Enter a name or email to search GitHub users.</p>
-      <input id="search-query" className="block px-2 py-1 mt-2 ml-0 mr-auto border rounded shadow-inner border-brown-800" type="text" placeholder="Name or email..."></input>
+      <input id="search-query"
+        className="block px-2 py-1 mt-2 ml-0 mr-auto border rounded shadow-inner border-brown-800"
+        onChange={() => { limitAndThrottle(results.limits[0], results.limits[1], 200, () => fetchGithub(document.getElementById("search-query").value), 500) }}
+        type="text"
+        placeholder="Name or email..."
+      />
       <Button className="mt-4" onClick={() => { fetchGithub(document.getElementById("search-query").value) }}>Search</Button>
-      <div id="results-total" className="block mt-4 ml-0 mr-auto"><span className="font-bold">Total Results: </span><span>{results.totalCount}</span></div>
       {results.overflow && <div id="results-overflow" className="block mt-2 ml-0 mr-auto"><p className="text-sm">Only the first {maxResults} search results are available from the GitHub API</p></div>}
       <div id="results-area" className="container mt-4">
         {results.items.length === 0 || <Pagination isTop={true} />}
-        {results.items.length === 0 ? <div>No results</div> :
-          <div className="container w-full overflow-x-auto">
-            <table className="w-full mt-10 text-sm text-center">
-              <thead><tr>{columns.map(col => <th className="-rotate-45 -translate-y-5" key={col}>{col}</th>)}</tr></thead>
-              <tbody>
-                {results.items.map(result => (
-                  <tr key={result.id}>
-                    <td className="border"><a href={result.html_url} target="_blank"><Image className="rounded" src={result.avatar_url} width="50" height="50"></Image></a></td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.login}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.name}</td>
-                    <td className="p-1 break-words border max-w-[150px]"><span className="text-brown-800"><a href={result.html_url + "?tab=repositories"}>{result.public_repos}</a></span></td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.followers}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.email}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.blog}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.company}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.location}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{result.bio}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{convertDate(result.updated_at)}</td>
-                    <td className="p-1 break-words border max-w-[150px]">{convertDate(result.created_at)}</td>
-                  </tr>
-                ))}</tbody></table></div>}
+        {results.totalCount === 0 && <div>No results.</div>}
+        {results.totalCount > 0 &&
+          <>
+            <div id="results-total" className="block mt-4 ml-0 mr-auto"><span className="font-bold">Total Results: </span><span>{results.totalCount}</span></div>
+            <div className="container w-full overflow-x-auto">
+              <table className="w-full mt-10 text-sm text-center">
+                <thead><tr>{columns.map(col => <th className="-rotate-45 -translate-y-5" key={col}>{col}</th>)}</tr></thead>
+                <tbody>
+                  {results.items.map(result => (
+                    <tr key={result.id}>
+                      <td className="border"><a href={result.html_url} target="_blank"><Image className="rounded" src={result.avatar_url} width="50" height="50"></Image></a></td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.login}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.name}</td>
+                      <td className="p-1 break-words border max-w-[150px]"><span className="text-brown-800"><a href={result.html_url + "?tab=repositories"}>{result.public_repos}</a></span></td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.followers}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.email}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.blog}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.company}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.location}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{result.bio}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{convertDate(result.updated_at)}</td>
+                      <td className="p-1 break-words border max-w-[150px]">{convertDate(result.created_at)}</td>
+                    </tr>
+                  ))}</tbody></table></div></>}
         {results.items.length === 0 || <Pagination isTop={false} />}
       </div>
     </Layout>
